f584cd4c25b61b550357dae9e4ca3e8e68060c6c
Anubhab Debnath <anubhabdebnath98@gmail.com>
2024-10-22 01:59:05 +0000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as zlib from "zlib";
+import * as crypto from "crypto";
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -7,6 +8,7 @@ const command = args[0];
 enum Commands {
   Init = "init",
   CatFile = "cat-file",
+  HashObject = "hash-object",
 }
 
 switch (command) {
@@ -18,10 +20,10 @@ switch (command) {
     console.log("Initialized git directory");
     break;
   case Commands.CatFile:
-    const hash = args[2];
+    const hashArg = args[2];
     // console.log(`Hash: ${hash}`);
 
-    const file = `.git/objects/${hash.slice(0, 2)}/${hash.slice(2)}`;
+    const file = `.git/objects/${hashArg.slice(0, 2)}/${hashArg.slice(2)}`;
     const buffer = fs.readFileSync(file);
     zlib.inflate(buffer, (err, inflated) => {
       if (err) {
@@ -33,6 +35,32 @@ switch (command) {
       process.stdout.write(content.trim());
     });
     break;
+
+  case Commands.HashObject:
+    const content = fs.readFileSync(args[2], "utf-8");
+    const header = `blob ${content.length}\0`;
+    const store = header + content;
+
+    const hash = crypto.createHash("sha1").update(store).digest("hex");
+    const dir = `.git/objects/${hash.slice(0, 2)}`;
+    const fileName = `${dir}/${hash.slice(2)}`;
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    zlib.deflate(store, (err, deflated) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      fs.writeFileSync(fileName, deflated);
+      process.stdout.write(hash);
+    });
+
+    break;
+
   default:
     throw new Error(`Unknown command ${command}`);
 }
